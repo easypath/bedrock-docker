b16b74037f344bcb56bd3380ca43c4cfe401d3f4
M Mahrous <m.mahrous@thed.io>
2017-01-15 23:23:54 +0200

    commit composer install /tmp

--- bedrock-docker.sh
+++ bedrock-docker.sh
@@ -3,28 +3,24 @@
 # Usage bedrock-docker.sh <domain_name>
 
 # Clone latest Bedrock Docker
-git clone https://github.com/thedgmbh/bedrock-docker.git $1 && rm -rf $1/.git
+# git clone https://github.com/thedgmbh/bedrock-docker.git $1 && rm -rf $1/.git
 
 # Clone latest Bedrock
 git clone --depth=1 https://github.com/roots/bedrock.git $1/site && rm -rf $1/site/.git
 
 # Read user wordpress connection input
 echo "Enter your database name [ENTER]: "
-read -r db_name
+read -r DB_NAME
 echo "Enter your database username [ENTER]: "
-read -r db_user
+read -r DB_USER
 echo "Enter your database password [ENTER]: "
-read -r db_pass
+read -r DB_PASSWORD
 echo "Enter your database prefix [ENTER]: "
-read -r db_prefix
+read -r DB_PREFIX
 
 # WordPress database connection details
 DB_FILE=.env
-DB_NAME=$db_name
-DB_USER=$db_user
-DB_PASSWORD=$db_pass
 DB_HOST=db:3306
-DB_PREFIX=$db_prefix
 WP_ENV=development
 WP_HOME=http://localhost:8080
 
@@ -43,5 +39,11 @@ sed -i -e "s,WP_HOME=http://example.com,WP_HOME=$WP_HOME,g" $1/site/$DB_FILE
 # Delete backup file
 rm $1/site/$DB_FILE-e
 
+# update envfile for docker compose
+echo "MYSQL_DATABASE=$DB_NAME" >> $1/$DB_FILE
+echo "MYSQL_USER=$DB_USER" >>  $1/$DB_FILE
+echo "MYSQL_ROOT_PASSWORD=$DB_PASSWORD" >> $1/$DB_FILE
+echo "MYSQL_PASSWORD=$DB_PASSWORD" >> $1/$DB_FILE
+
 # Run Docker build
-(cd $1 && docker-compose up --build -d)
+(cd $1 && docker-compose up --build)
